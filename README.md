6c9bad7f730375d8804a66136f07ed77ae04850a
Aleks Rūtiņš <keeper@farthergate.com>
2025-12-27 15:05:24 -0500

    Add metadata

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Pacuare
 
-**TODO: Add description**
+An Elixir client for the Pacuare SDK.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `pacuare_sdk` to your list of dependencies in `mix.exs`:
+This package can be installed by adding `pacuare_sdk` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -15,7 +14,4 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/pacuare_sdk>.
-
+Documentation can be found at <https://hexdocs.pm/pacuare_sdk>.
--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Pacuare.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "An Elixir client for the Pacuare SDK.",
+      name: "Pacuare",
+      source_url: "https://github.com/pacuare/sdk-ex",
+      package: package()
     ]
   end
 
@@ -26,6 +30,21 @@ defmodule Pacuare.MixProject do
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "pacuare",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/pacuare/sdk-ex",
+        "Website" => "https://pacuare.dev"
+      }
     ]
   end
 end
