2f18828207ef979313282e004b4b3cb01271603e
Aleks Rūtiņš <keeper@farthergate.com>
2025-12-27 15:12:52 -0500

    Fix app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pacuare.MixProject do
 
   def project do
     [
-      app: :pacuare_sdk,
+      app: :pacuare,
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
