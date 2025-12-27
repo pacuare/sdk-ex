0c5a165b31c252b82150aa2921e3d9db172d178b
Aleks Rūtiņš <keeper@farthergate.com>
2025-12-27 15:07:07 -0500

    Add license

--- mix.exs
+++ mix.exs
@@ -38,8 +38,7 @@ defmodule Pacuare.MixProject do
       # This option is only needed when you don't want to use the OTP application name
       name: "pacuare",
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["MIT"],
       links: %{
         "GitHub" => "https://github.com/pacuare/sdk-ex",
